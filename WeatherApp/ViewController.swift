0cbd7034c72ed82978fc4a4349a4ca3643bd34c0
Antonio-Flores1 <antonioflores2280@gmail.com>
2023-07-09 22:08:24 -0400

    adding UItableview delegate and Datasource

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
 
 
