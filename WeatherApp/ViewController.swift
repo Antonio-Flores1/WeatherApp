8c9c2ccac5504ee573afa5c02769d32e9e637b21
Antonio-Flores1 <antonioflores2280@gmail.com>
2023-07-18 20:18:42 -0400

    cleaned up code and URL session, will attempt to get data with remodeled weather link and Weather model decodable

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet var table: UITableView!
     
-    var weatherModel = [dogApiTest]()
+    var weatherModel = [Weather]()
     let locationManager = CLLocationManager()
     var currentLocations: CLLocation?
     
@@ -49,10 +49,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let long = currentLocations.coordinate.longitude
         let lat = currentLocations.coordinate.latitude
 
-//    https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=fm3FfjFr9iuu6ZQ3PrsBi5NdBkQ700EL
-   let urlString = "https://dog.ceo/api/breeds/image/random"
-        
-//        "https://api.tomorrow.io/v4/timelines?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=fm3FfjFr9iuu6ZQ3PrsBi5NdBkQ700EL"
+        let urlString = "https://api.tomorrow.io/v4/weather/forecast?location=40.75872069597532,-73.98529171943665&fields=temperature&timesteps=1h&units=metric&apikey=fm3FfjFr9iuu6ZQ3PrsBi5NdBkQ700EL"
         
         guard let url = URL(string: urlString) else {
             print("Issues with URL")
@@ -65,58 +62,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 print(error?.localizedDescription ?? "unsure of error description")
                 return
             }
-            
+
             do {
-                let json = try JSONDecoder().decode(dogApiTest.self, from: data)
-//                DispatchQueue.global().async {
+                let json = try JSONDecoder().decode(Weather.self, from: data)
                     DispatchQueue.main.async {
-                        dump(json)
-                        dump(dogApiTest.self)
-                        
+                        dump(json.timelines)
+                        dump(Weather.self)
                 }
             } catch {
                 print(error)
             }
         }
-            task.resume()
-    
-//      "https://api.tomorrow.io/v4/timelines?location=\(lat),\(long)&fields=temperature&timesteps=1h&units=metric&apikey=fm3FfjFr9iuu6ZQ3PrsBi5NdBkQ700EL"
-//        let urlRequest = URLRequest(url: url!)
-    
-        
-//        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-//
-////        }
-//
-//            guard let weatherData = data, error == nil else {
-//                print("Something didn't happen")
-//                return
-//            }
-//
-//            do {
-//                 let weather = try JSONDecoder().decode( Weather.self, from: weatherData)
-//                    DispatchQueue.main.async {
-//                        self.weatherModel = [weather]
-//    //                    self.table.reloadData()
-//                        print("\(weather)")
-//                        dump(weather)
-//    //                    self!.weatherModel = [weather]
-//                    }
-////                completionHandler(weather,nil)
-//
-//            } catch {
-//                print("Error: \(error)")
-//            }
-//
-//
-//            guard let error = error else {
-//                print("Error: \(error)")
-//                return
-//            }
-//
-//
-//        }.resume()
-        
+        task.resume()
     }
 
         
@@ -141,33 +98,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 }
 
-struct dogApiTest: Codable{
-    let message: String
-    let status: String
-}
-
 
 
 struct Weather: Codable {
-    let data: DataClass?
+    let timelines: Weatherhourly?
 }
 
-struct DataClass: Codable {
-    let timelines: [Timeline]?
+struct Weatherhourly: Codable {
+    let hourly: [WeatherData]?
 }
 
-struct Timeline: Codable {
-    let timestep: String?
-    let endTime: String?
-    let startTime: String?
-    let intervals: [IntervalsWeather]?
-}
-
-struct IntervalsWeather: Codable {
-    let startTime: String?
-    let values: [TemperatureWeather]?
-}
-
-    struct TemperatureWeather: Codable {
-    let temperature: Float?
+struct WeatherData: Codable {
+    let time: String?
+    let values: String?
 }
